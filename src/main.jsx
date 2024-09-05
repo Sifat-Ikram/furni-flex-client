e6cc81c5f32fe40ad9b4887e4374af3df444cefd
Sifat-Ikram <sifatikram@gmail.com>
2024-09-05 12:51:20 +0600

    building website started

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/pages/home/Home.jsx';
+import Login from './components/pages/login/Login.jsx';
+import SignUp from './components/pages/signUp/SignUp.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/signUp",
+        element: <SignUp />
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
